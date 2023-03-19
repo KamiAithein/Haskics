9c72d11b41844ee4eef42b5fe1294414b7b028bc
Mayo <mayo@Mayos-MacBook-Pro.local>
2023-03-19 09:49:35 -0500

    mve complete

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,33 @@
 module Main (main) where
 
-import Image as I
-import Draw as D
-import Data.Array.IArray
-import Prelude as P
+import Data.Array
+import Data.Array.ST
 
-type Canvas = RawImage
+import Control.Monad
+import Control.Monad.ST
 
+data Image s = Image 
+    { imgImg :: STArray s Int Int
+    }
 
+setAllOnes :: Image s -> ST s ()
+setAllOnes img = do
+    (_, hi) <- getBounds $ imgImg img
+    forM_ [1..hi] (\i -> writeArray (imgImg img) i 1) 
 
-rectangleExample :: Canvas -> Canvas
-rectangleExample canvas = 
-    let canvas'  = D.fillRectWith (0, 0) 50 50 (0, 255, 0, 255) canvas
-        canvas'' = D.fillRectWith (50, 50) 50 50 (0, 0, 255, 200) canvas'
-    in D.fillRectWith (0, 0) 100 100 (255, 0, 0, 100) canvas''
--- fillRectWith :: Point -> Int -> Int -> I.RawPixel -> I.RawImage -> I.RawImage
+setHalfTwos :: Image s -> ST s ()
+setHalfTwos img = do
+    (_, hi) <- getBounds $ imgImg img
+    forM_ [1..hi `div` 2] (\i -> writeArray (imgImg img) i 2)
 
-linesExample :: Canvas -> Canvas
-linesExample canvas = 
-
-    let canvas' = D.fillWith (255, 0, 0, 0) canvas
-        strokeWidth = 1
-        commands = 
-            [ ((0, 0),  (255, 255),  strokeWidth, (0, 0, 0, 255))
-            , ((0, 255),  (255, 0),  strokeWidth, (0, 0, 0, 255))
-            , ((0, 200),  (255, 55), strokeWidth, (0, 0, 0, 255))
-            , ((0, 55),  (255, 200), strokeWidth, (0, 0, 0, 255))
-            , ((55, 0),  (200, 255), strokeWidth, (0, 0, 0, 255))
-            , ((200, 0),  (55, 255), strokeWidth, (0, 0, 0, 255))
-            , ((0, 200), (255, 200), strokeWidth, (0, 0, 0, 255))
-            ]
-    in P.foldl (\acc (p1, p2, width, color) -> D.fillLineWith p1 p2 width color acc) canvas' commands
-
-triangleExample :: Canvas -> Canvas
-triangleExample canvas  = D.fillTriangleWith (100, 0) (100, 100) (0, 100) (0, 255, 0, 255) canvas
-
-triangleExample2 :: Canvas -> Canvas
-triangleExample2 canvas = 
-    let canvas' = D.fillTriangleWith (100, 0) (100, 100) (0, 100) (0, 255, 0, 255) canvas
-    in D.fillTriangleWith (100, 0) (255, 255) (0, 100) (255, 0, 255, 125) canvas'
-
-circleExample :: Canvas -> Canvas
-circleExample canvas = D.fillCircleWith (120, 120) 50 (255, 0, 0, 255) canvas
-
-run :: IO ()
-run = do
-    let width = 255
-    let height = 255
-    let image = I.createRawImage width height
-    -- let image' = linesExample image
-    -- let image' = triangleExample image
-    let image' = triangleExample2 image
-    -- let image' = rectangleExample image
-    -- let image' = circleExample image
-    saveRawImage image' "assets/test.ppm"
-    putStrLn "pottato"
 
 main :: IO ()
-main = run
+main = 
+    let arr = runSTArray $ do 
+            arr <- thaw $ array (1, 10) [(i, 0) | i <- [1..10]] :: ST s (STArray s Int Int)
+            let img = Image {imgImg=arr}
+            setAllOnes img
+            setHalfTwos img
+            return arr
+    in do
+            forM_ arr (\val -> putStrLn $ show val)
